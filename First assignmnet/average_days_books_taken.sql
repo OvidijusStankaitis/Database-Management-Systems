569928ed9ce8165b90ac0a170fd7f27cb84f0911
OvidijusStankaitis <ovidijus.stankaitis@gmail.com>
2023-10-13 10:12:45 +0300

    Finished first assignment

--- First assignmnet/average_days_books_taken.sql
+++ First assignmnet/average_days_books_taken.sql
@@ -1,3 +1,6 @@
 SELECT AVG(grazinti - paimta) AS avg_days
 FROM Stud.Egzempliorius
-WHERE paimta IS NOT NULL AND grazinti IS NOT NULL
+    -- WHERE paimta IS NOT NULL AND 
+    --     grazinti IS NOT NULL
+
+-- IF NEEDED ::NUMERIC(10,3)
---
+++ First assignmnet/specific_reader_books.sql
@@ -0,0 +1,7 @@
+SELECT Stud.Egzempliorius.nr, pavadinimas
+FROM Stud.Skaitytojas
+    INNER JOIN Stud.Egzempliorius ON Stud.Egzempliorius.skaitytojas = Stud.Skaitytojas.nr
+    INNER JOIN Stud.Knyga ON Stud.Knyga.isbn = Stud.Egzempliorius.isbn
+    WHERE vardas = 'Jonas' AND 
+        pavarde = 'Petraitis' AND 
+        paimta IS NOT NULL; 
